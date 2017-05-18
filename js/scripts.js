62847b03ba87041c093b0566cbe90744598cca2f
rockstarchiku <jngamau93@gmail.com>
2017-05-18 20:16:08 +0300

    Added form in modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function() {
    $("input#toyota").click(function() {
      $("div#display").fadeIn("slow");
@@ -24,7 +25,43 @@ $(document).ready(function() {
      $("div#display").fadeIn("slow");
      $("div#display1").fadeIn("slow");
    });
+
+   $(".user form").submit(function(event) {
+
+     var fullName = $("input#fullname").val();
+     var address = $("input#address").val();
+     var email = $("input#email").val();
+
+
+     $(".fullname").text(fullName);
+     $(".address").text(address);
+     $(".email").text(email);
+
+
+     $("#receipt").show();
+     $(".user").hide();
+
+     event.preventDefault();
+
 });
+});
+
+function updateTotal() {
+ document.getElementById("amtOut").innerHTML=document.getElementById("amtIn").value;
+
+}
+function applyDiscount(amount) {
+  var amount = parseInt(20,000);
+  var disAmt = 1.16*(amount);
+  document.getElementById("disOut").innerHTML = disAmt;
+  // document.getElementById("disOut").innerHTML = cashIn;
+  var tot = (disAmt)*0.02 + (disAmt);
+
+  document.getElementById("totOut").innerHTML=tot;
+}
+
+
+
 // Activate Carousel
 $("#myCarousel").carousel();
 
