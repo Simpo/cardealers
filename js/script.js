616e72675ec67e3ccb7cdb82da815d816ad9509e
Simpo <marcusgathii2011@gmail.com>
2017-05-18 16:32:23 +0300

    js update

--- js/script.js
+++ js/script.js
@@ -8,9 +8,20 @@ function updateTotal() {
 
 }
 function applyDiscount(amount) {
-  amount = parseFloat(amount);
+  amount = parseInt(amount);
   var discount = 16;
   var disAmt = 1.16*(amount);
   document.getElementById("disOut").innerHTML = disAmt;
+  // document.getElementById("disOut").innerHTML = cashIn;
+  var tot = (disAmt)*0.02 + (disAmt);
+
+  document.getElementById("totOut").innerHTML=tot;
+
 
 }
+// function upTotal(total) {
+//   total = parseFloat(total);
+//   var tot = (disAmt)*0.02
+//   document.getElementById("totOut").innerHTML=tot;
+//
+// }
